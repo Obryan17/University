ef34f19685bb5b37b77fb12280557e5af9e19bfa
aw <graceawoute17@gmail.com>
2024-04-03 14:24:24 +0200

    code source

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+import ocean from './images/ocean.jpg'
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+         <img src='src/images/ocean.jpg' alt='ocean.jpg'/>
+      </div>
+    );
+  }
+
+
+}
+
+export default App;
